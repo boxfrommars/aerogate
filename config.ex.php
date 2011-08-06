ad24c1272dae338044923f9d077f7f009cd66f0f
Dmitry Groza <boxfrommars@gmail.com>
2011-08-06 21:21:25 +0400

    config example updated

--- config.ex.php
+++ config.ex.php
@@ -8,8 +8,8 @@ $config = array(
  		'galileo' => array(
  			'client_id' => '1',
  			'url' => 'https://example.com/',
-            'server_public_key' => 'file://' . __DIR__ . '/ServerKeyPublic.pem',
-            'client_private_key' => 'file://' . __DIR__ . '/ClientKeyPrivate.pem',
+			'server_public_key' => 'file://' . __DIR__ . '/ServerKeyPublic.pem',
+			'client_private_key' => 'file://' . __DIR__ . '/ClientKeyPrivate.pem',
 		),
 	)
 );
