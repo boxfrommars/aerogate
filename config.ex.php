c6e3941452a8cdfb1f7f240b088542b84917cd5d
Dmitry Groza <boxfrommars@gmail.com>
2011-08-06 21:19:54 +0400

    config example updated

--- config.ex.php
+++ config.ex.php
@@ -3,11 +3,13 @@ $config = array(
 	'gateways' => array(
 		'siren' => array(
 			'password' => 'password',
-			'url' => '127.0.0.1'
+			'url' => 'https://example.com/'
 		),
  		'galileo' => array(
  			'client_id' => '1',
-			'url' => '127.0.0.1',
+ 			'url' => 'https://example.com/',
+            'server_public_key' => 'file://' . __DIR__ . '/ServerKeyPublic.pem',
+            'client_private_key' => 'file://' . __DIR__ . '/ClientKeyPrivate.pem',
  		),
 	)
 );
